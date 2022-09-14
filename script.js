ec3630c1cc64f2776945304cd1a755ea3d6be9c4
Marquell <starjet164@gmail.com>
2022-09-14 12:54:30 -0500

    landing page

--- script.js
+++ script.js
@@ -6,25 +6,25 @@ const options = {
   }
 };
 
-let imgsrc;
-let objectURL;
+// let imgsrc;
+// let objectURL;
 
-let imageLookEl = document.querySelector("#image-look")
+// let imageLookEl = document.querySelector("#image-look")
 
-fetch('https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact', options)
-  .then(response => response.blob())
-  .then(imageBlob => {
-    imgsrc = imageBlob;
-    const objectURL = URL.createObjectURL(imageBlob)
-    console.log(objectURL)
+// fetch('https://ronreiter-meme-generator.p.rapidapi.com/meme?top=Top%20Text&bottom=Bottom%20Text&meme=Condescending-Wonka&font_size=50&font=Impact', options)
+//   .then(response => response.blob())
+//   .then(imageBlob => {
+//     imgsrc = imageBlob;
+//     const objectURL = URL.createObjectURL(imageBlob)
+//     console.log(objectURL)
 
-    imageLookEl.src = objectURL
+//     imageLookEl.src = objectURL
 
 
 
-  })
+//   })
 
-  .catch(err => console.error(err));
+//   .catch(err => console.error(err));
 
   fetch('https://ronreiter-meme-generator.p.rapidapi.com/images', options)
 	.then(response => response.json())
